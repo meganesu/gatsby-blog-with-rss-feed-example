6b915efcb2de1f137bb19264a63e6aa6bd14e56e
Megan Sullivan <megan@gatsbyjs.com>
2021-01-29 00:57:23 -0800

    step 4: add link to RSS feed to the home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { graphql, Link } from "gatsby"
 
 const IndexPage = ({ data }) => {
   return (
-    <main
+    <div
       style={{
         fontFamily: "Helvetica, sans-serif",
         margin: "auto",
@@ -11,36 +11,34 @@ const IndexPage = ({ data }) => {
         padding: "1rem 2rem",
       }}
     >
-      <title>My Personal Blog</title>
-      <h1 style={{ color: "rebeccapurple" }}>
-        My Blog Posts
-      </h1>
-      <ul
-        style={{
-          padding: 0,
-          marginTop: "3rem",
-        }}
-      >
-        {
-          data.allMarkdownRemark.nodes.map(post => (
+      <main>
+        <title>My Personal Blog</title>
+        <h1 style={{ color: "rebeccapurple" }}>My Blog Posts</h1>
+        <ul
+          style={{
+            padding: 0,
+            marginTop: "3rem",
+          }}
+        >
+          {data.allMarkdownRemark.nodes.map((post) => (
             <li
               style={{
                 listStyle: "none",
               }}
             >
-              <Link
-                to={post.slug}
-                style={{ color: "black" }}
-              >
-                <h2>
-                  {post.frontmatter.title}
-                </h2>
+              <Link to={post.slug} style={{ color: "black" }}>
+                <h2>{post.frontmatter.title}</h2>
               </Link>
             </li>
-          ))
-        }
-      </ul>
-    </main>
+          ))}
+        </ul>
+      </main>
+      <footer>
+        <Link to="/rss.xml" style={{ marginTop: "2rem" }}>
+          RSS feed
+        </Link>
+      </footer>
+    </div>
   )
 }
 
