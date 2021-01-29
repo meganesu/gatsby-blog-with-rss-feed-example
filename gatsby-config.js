e373d46d2ab9c3af780b508c101f2b0609cb9de7
Megan Sullivan <megan@gatsbyjs.com>
2021-01-29 00:54:15 -0800

    step 2: configure gatsby-plugin-feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,52 @@ module.exports = {
       "An example Gatsby blog with an automatically generated RSS feed.",
   },
   plugins: [
+    {
+      resolve: "gatsby-plugin-feed",
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            title: "My Personal Blog RSS Feed",
+            output: "rss.xml",
+            query: `
+            {
+              allMarkdownRemark(sort: {fields: frontmatter___date, order: ASC}) {
+                nodes {
+                  frontmatter {
+                    title
+                    date
+                    description
+                  }
+                  html
+                  slug: gatsbyPath(filePath: "/{MarkdownRemark.frontmatter__title}")
+                }
+              }
+            }
+            `,
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.nodes.map(node => {
+                return Object.assign({}, node.frontmatter, {
+                  url: `${site.siteMetadata.siteUrl}${node.slug}`,
+                  guid: `${site.siteMetadata.siteUrl}${node.slug}`,
+                })
+              })
+            },
+          }
+        ]
+      }
+    },
     "gatsby-transformer-remark",
     {
       resolve: "gatsby-source-filesystem",
